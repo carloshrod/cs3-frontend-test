180ddd539d0a350d488f863e4cbc5a93c836b939
chrod <chrod.dev@gmail.com>
2024-03-21 13:50:37 -0500

    Initial settings

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import '@/styles/globals.scss';
+import { theme } from '@/theme';
+import { ThemeProvider } from '@mui/material';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<ThemeProvider theme={theme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
